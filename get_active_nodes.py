a7450d5dd59d8df45be8440d00aa7ee7bff3a46b
hapax.io <39811582+thehapax@users.noreply.github.com>
2019-07-21 21:28:20 -0700

    update formatting

--- get_active_nodes.py
+++ get_active_nodes.py
@@ -12,7 +12,7 @@ max_timeout = 2.0
 def pretty_print(df):
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         # pretty print entire thing
-        print(df)
+        print(df.to_string(index=False))
 
 def wss_test(node):
     """
@@ -32,8 +32,9 @@ def fetch_node_latency(node):
     name = mp.current_process().name
     node_info = dict()
     latency = wss_test(node)
-#    print(f"{name}: {node}, latency: {latency}")
-    node_info = {'node': node, 'latency': latency}
+    #print(f"{name}: {node}, latency: {latency}")
+    print('#', end='', flush=True)
+    node_info = {'Node': node, 'Latency': latency}
     return node_info
 
         
@@ -42,6 +43,8 @@ if __name__ == '__main__':
     nodelist = public_nodes()
     pool_size = mp.cpu_count()*2
     latency_info = []
+
+    print("Polling nodes...")
     
     with mp.Pool(processes=pool_size) as pool:
         latency_info = pool.map(fetch_node_latency, nodelist)
@@ -49,12 +52,12 @@ if __name__ == '__main__':
     pool.close()
     pool.join()
 
+    print(" - 100%\n")
     print("Active nodes within your range:")
     
     df = pd.DataFrame(latency_info)
     dfc = df.dropna()
-    dfc = dfc.sort_values('latency', ascending=True)
-    dfc = dfc.reset_index()
+    dfc = dfc.sort_values('Latency', ascending=True)
     pretty_print(dfc)
 
     
